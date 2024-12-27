5b2399a9de99afae5410f974d6818c0d7cdb23ce
stigl <realstigl@gmail.com>
2024-12-27 23:11:17 +0100

    Fix the naming :D

--- dumbparens.el
+++ dumbparens.el
@@ -1,3 +1,41 @@
+;;; dumbparens.el --- Upload sharing to 0x0.st -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 William Vaughn
+;;
+;; Author: William Vaughn <vaughnwilld@gmail.com>
+;; Maintainer: William Vaughn <vaughnwilld@gmail.com>
+;; Created: June 26, 2021
+;; Modified: August 16, 2023
+;; Version: 1.0.1
+;; Homepage: https://git.sr.ht/~willvaughn/emacs-0x0
+;; Package-Requires: ((emacs "26.1"))
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; Integration with https://0x0.st, envs.sh, ttm.sh, and self-hosted services
+;;
+;; Upload whatever you need to a pastebin server. The commands
+;; `0x0-upload-file',`0x0-upload-text' and `0x0-upload-kill-ring', which
+;; respectively upload (parts of) the current buffer, a file on your disk and a
+;; string from the minibuffer can be used too.
+;;
+;; The default host and the host this package is named after is https://0x0.st.
+;; Consider donating to https://liberapay.com/mia/donate if you like the service.
+
+
 ;; DumbParens
 ;;
 ;; Due to the currently ongoing issue of doing pairs propertly in smartparens this had to be made
